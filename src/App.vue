469ca05427963ca3ddb517ccdfe997fd7fe2e768
Lambertn33 <lambertgithub@gmail.com>
2023-12-21 20:56:34 +0200

    feat: prepare components

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <h2>Tracker</h2>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionsList />
+    <TransactionsForm />
+  </div>
 </template>
+
+<script setup>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionsForm from "./components/TransactionsForm.vue";
+import TransactionsList from "./components/TransactionsList.vue";
+</script>
